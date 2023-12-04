fb0dff9175d91935b2762904c465e76f041a9157
Chris Manson <chris@manson.ie>
2023-12-04 21:40:53 +0000

    run lint --fix

--- src/gather-changes.ts
+++ src/gather-changes.ts
@@ -1,12 +1,16 @@
 import execa from 'execa';
-import {resolve} from 'path';
+import { resolve } from 'path';
 import { dirname } from 'path';
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
 export async function gatherChanges() {
   const lernaChangelogPath = require.resolve('@ef4/lerna-changelog');
-  
-  let result = await execa('node', [resolve(dirname(lernaChangelogPath), 'bin', 'cli.js'), '--next-version', 'Release']);
+
+  const result = await execa('node', [
+    resolve(dirname(lernaChangelogPath), 'bin', 'cli.js'),
+    '--next-version',
+    'Release',
+  ]);
   return result.stdout;
 }
--- src/interdep.ts
+++ src/interdep.ts
@@ -15,56 +15,68 @@ export interface PkgEntry {
 }
 
 export function publishedInterPackageDeps(): Map<string, PkgEntry> {
-  let rootDir = './';
-  let packages: Map<string, PkgEntry> = new Map();
+  const rootDir = './';
+  const packages: Map<string, PkgEntry> = new Map();
 
   function loadPackage(packagePath: string) {
-    let pkg = readJSONSync(packagePath);
-      if (pkg.private) {
-        return;
-      }
-      pkgJSONS.set(pkg.name, pkg);
-      packages.set(pkg.name, {
-        version: pkg.version,
-        pkgJSONPath: `./${relative('.', packagePath)}`,
-        isDependencyOf: new Map(),
-        isPeerDependencyOf: new Map(),
-      });
+    const pkg = readJSONSync(packagePath);
+    if (pkg.private) {
+      return;
+    }
+    pkgJSONS.set(pkg.name, pkg);
+    packages.set(pkg.name, {
+      version: pkg.version,
+      pkgJSONPath: `./${relative('.', packagePath)}`,
+      isDependencyOf: new Map(),
+      isPeerDependencyOf: new Map(),
+    });
   }
 
-  let pkgJSONS: Map<string, any> = new Map();
+  const pkgJSONS: Map<string, any> = new Map();
 
-  if(!existsSync('./pnpm-workspace.yaml')) {
-    const result = execa.sync("npm", ["query", ".workspace"]);
+  if (!existsSync('./pnpm-workspace.yaml')) {
+    const result = execa.sync('npm', ['query', '.workspace']);
     const resultParsed = JSON.parse(result.stdout);
-    const locations = resultParsed.map((i:any) => i.location);
+    const locations = resultParsed.map((i: any) => i.location);
 
-    for( const location of locations ) {
+    for (const location of locations) {
       loadPackage(resolve(location, 'package.json'));
     }
 
     loadPackage('./package.json');
   } else {
-    for (let pattern of (yaml.load(readFileSync('./pnpm-workspace.yaml', 'utf8')) as any)
-      .packages) {
-      for (let dir of glob.sync(pattern, { cwd: rootDir, expandDirectories: false, onlyDirectories: true })) {
-        loadPackage(resolve(rootDir, dir, 'package.json'))
+    for (const pattern of (
+      yaml.load(readFileSync('./pnpm-workspace.yaml', 'utf8')) as any
+    ).packages) {
+      for (const dir of glob.sync(pattern, {
+        cwd: rootDir,
+        expandDirectories: false,
+        onlyDirectories: true,
+      })) {
+        loadPackage(resolve(rootDir, dir, 'package.json'));
       }
     }
   }
 
-  for (let [consumerName, consumerPkgJSON] of pkgJSONS) {
+  for (const [consumerName, consumerPkgJSON] of pkgJSONS) {
     // no devDeps because changes to devDeps shouldn't ever force us to
     // release
-    for (let section of ['dependencies', 'peerDependencies'] as const) {
+    for (const section of ['dependencies', 'peerDependencies'] as const) {
       if (consumerPkgJSON[section]) {
-        for (let [depName, depRange] of Object.entries(consumerPkgJSON[section] as Record<string, string>)) {
+        for (const [depName, depRange] of Object.entries(
+          consumerPkgJSON[section] as Record<string, string>,
+        )) {
           if (depRange.startsWith('workspace:')) {
-            let dependency = packages.get(depName);
+            const dependency = packages.get(depName);
             if (!dependency) {
-              throw new Error(`broken "workspace:" reference to ${depName} in ${consumerName}`);
+              throw new Error(
+                `broken "workspace:" reference to ${depName} in ${consumerName}`,
+              );
             }
-            let field = section === 'dependencies' ? ('isDependencyOf' as const) : ('isPeerDependencyOf' as const);
+            const field =
+              section === 'dependencies'
+                ? ('isDependencyOf' as const)
+                : ('isPeerDependencyOf' as const);
             dependency[field].set(consumerName, depRange as Range);
           }
         }
