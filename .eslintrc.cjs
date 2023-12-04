ccb45610771842dfa032ec4f9a18ef78f1a2725d
Chris Manson <chris@manson.ie>
2023-12-04 22:20:07 +0000

    add shebang back and fix linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,5 +19,13 @@ module.exports = {
   rules: {
     'n/no-process-exit': 0,
     '@typescript-eslint/no-explicit-any': 0,
+    'n/shebang': [
+      'error',
+      {
+        convertPath: {
+          'src/*.ts': ['^src/(.+).ts$', 'dist/$1.js'],
+        },
+      },
+    ],
   },
 };
